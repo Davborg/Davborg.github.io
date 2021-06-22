e60090463f75b5c48a09b1a3e1722083928ab019
Davborg <David.borger82@gmail.com>
2021-06-22 15:09:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ contract = new web3.eth.Contract(abi, contractAddress);
 function registerGetInfo() {
 	//document.getElementById('lastInfo').innerHTML = "test2";
    //contract.methods.retrieve().call().then( function( info ) {
-	var message = "123";  
-    contract.methods.retrieve().call().then(message);
+	var message = 124;  
+    //contract.methods.retrieve().call().then(message);
 	document.getElementById('lastInfo').innerHTML = message;
   //});    
 }
