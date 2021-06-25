fefdc0c54b033468b2c94d836b34b6b4bfa44563
Davborg <David.borger82@gmail.com>
2021-06-25 19:18:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Source code to interact with smart contract
 
-const Web3 = require('web3');
+//Web3 = require('web3');
 
 //connection with node
 var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/537393d11b444181a5f4c81702e0be5e'));
@@ -11,11 +11,11 @@ web3.eth.net.isListening()
 
 
 // contractAddress and abi are setted after contract deploy
-var contractAddress = '0x567E2A8E4Df3845dEC5327F634c2c16EdbA2E1aA';
-var abi = JSON.parse( '[{"inputs": [{"internalType": "uint256","name": "num","type": "uint256"}],"name": "store","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "retrieve","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"}]' );
+//var contractAddress = '0x567E2A8E4Df3845dEC5327F634c2c16EdbA2E1aA';
+//var abi = JSON.parse( '[{"inputs": [{"internalType": "uint256","name": "num","type": "uint256"}],"name": "store","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "retrieve","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"}]' );
 
 //contract instance
-var contract = new web3.eth.Contract(abi, contractAddress);
+//var contract = new web3.eth.Contract(abi, contractAddress);
 
 // Accounts
 //var account;
