722aadcdae7055970e5f2031a7d985f86ddd87bd
Davborg <David.borger82@gmail.com>
2021-06-25 19:08:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Source code to interact with smart contract
 
+const Web3 = require('web3');
+
 //connection with node
 var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/537393d11b444181a5f4c81702e0be5e'));
+
+web3.eth.net.isListening()
+   .then(() => alert('web3 is connected'))
+   .catch(e => alert('Wow. Something went wrong'));
+
 
 // contractAddress and abi are setted after contract deploy
 var contractAddress = '0x567E2A8E4Df3845dEC5327F634c2c16EdbA2E1aA';
@@ -20,7 +27,7 @@ function registerGetInfo() {
 	document.getElementById('lastInfo').innerHTML = "test2";
    //contract.methods.retrieve().call().then( function( info ) {
 	  // var provider = web3.eth.currentProvider;
-	   contract.methods.retrieve().call().then( function( info ) { 
+	   //contract.methods.retrieve().call().then( function( info ) { 
 		alert(info);
 		document.getElementById('lastInfo').innerHTML = String(info);
 
